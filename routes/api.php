7b323b9a303060825748900a5d93389d272ef668
Chornchoan <chorn.choan@student.passerellesnumeriques.org>
2024-06-11 14:16:43 +0700

    add code to logout

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-
 Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
