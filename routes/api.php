03e99123ae978b6c777fe853250b6555c689c6ba
RATANAK.KHOEURN <RATANAK.KHOEURN@PNC.LAN>
2024-06-15 16:33:24 +0700

    fix route api

--- routes/api.php
+++ routes/api.php
@@ -31,24 +31,13 @@ Route::middleware('auth:sanctum')->group(function() {
    Route::post('/share-post',[ShareController::class, 'sharePost']);
    Route::get('/get-post/{id}',[PostController::class, 'getPosts']);
    Route::get('/get-share/{id}', [ShareController::class, 'getShare']);
+});
   
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 //profile
-
-// Route::get('/profile/getProfilePhoto', [ProfileController::class, 'getProfilePhoto']);
 Route::post('/profile/update-profile',[ProfileController::class,'update_profile'])->middleware('auth:sanctum');
-
-
 Route::get('/user/list', );
-Route::middleware('auth:sanctum')->group(function () {
-   Route::post('/add-post', [PostController::class, 'addPost']);
-   Route::post('/add-comment', [CommentController::class, 'addComment']);
-   Route::post('/add-like', [PostController::class, 'addLike']);
-   Route::get('/get-post/{id}', [PostController::class, 'getPosts']);
-});
