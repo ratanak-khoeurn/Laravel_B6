4debf367f5bde5dd16778981887a57995379a330
SOKUNTHEA.POEUN <SOKUNTHEA.POEUN@PNC.LAN>
2024-06-16 21:11:39 +0700

    list all post

--- routes/api.php
+++ routes/api.php
@@ -18,17 +18,22 @@ use App\Http\Controllers\Share\ShareController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/post/list', [PostController::class, 'indexPost'])->name('post.list');
 
-Route::middleware('auth:sanctum')->group(function() {
-   Route::post('/add-post',[PostController::class, 'addPost']);
-   Route::post('/add-comment',[CommentController::class, 'addComment']);
-   Route::post('/add-like',[PostController::class, 'addLike']);
-   Route::post('/share-post',[ShareController::class, 'sharePost']);
-   Route::get('/get-post/{id}',[PostController::class, 'getPosts']);
+Route::middleware('auth:sanctum')->group(function () {
+   Route::post('/add-post', [PostController::class, 'addPost']);
+   // Route::post('/post/create', [PostController::class, 'storePost'])->name('post.create');
+   Route::get('/post/show/{id}', [PostController::class, 'showPost'])->name('post.show');
+   Route::delete('/post/delete/{id}', [PostController::class, 'deletePost'])->name('post.destroy');
 
+
+   Route::post('/add-comment', [CommentController::class, 'addComment']);
+   Route::post('/add-like', [PostController::class, 'addLike']);
+   Route::post('/share-post', [ShareController::class, 'sharePost']);
+   Route::get('/get-post/{id}', [PostController::class, 'getPosts']);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
